2c39bf88d081e38ddea761f18daee0524e9e6903
Algonacci <algonacci1@gmail.com>
2022-02-05 23:51:57 +0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:cozy_app/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,70 @@
+import 'package:cozy_app/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset('assets/splash_image.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 50,
+                left: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/logo.png'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    'Find Cozy House\nto Stay and Happy',
+                    style: blackTextStyle.copyWith(fontSize: 24),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                    style: greyTextStyle,
+                  ),
+                  SizedBox(height: 40),
+                  Container(
+                    height: 50,
+                    width: 210,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          primary: purpleColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(17),
+                          )),
+                      child: Text(
+                        'Explore Now',
+                        style: whiteTextStyle.copyWith(fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
